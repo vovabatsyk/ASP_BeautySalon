80e5c78eeb243b102931576d42f3d099f0a31f0a
Volodymyr Batsyk <vovabatsyk.lviv@gmail.com>
2021-12-02 21:47:48 +0200

    Initial

---
+++ BeautySalon/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BeautySalon.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
+
