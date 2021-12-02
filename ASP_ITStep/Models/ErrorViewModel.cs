8c8a8aa65b2965290127cd93671ed089be55f47d
vovabatsyk <vovabatsyk.lviv@gmail.com>
2021-12-02 18:39:24 +0200

    Initial

---
+++ ASP_ITStep/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ASP_ITStep.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
