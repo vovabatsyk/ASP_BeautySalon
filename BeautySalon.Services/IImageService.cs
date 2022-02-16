9aa0090bfae6e2b8c7aa6b06ab615de5900843a2
Volodymyr Batsyk <vovabatsyk.lviv@gmail.com>
2022-02-16 22:04:35 +0200

    Upload Image to Azure Storage

---
+++ BeautySalon.Services/IImageService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BeautySalon.Services
+{
+    public interface IImageService
+    {
+        Task<string> GetImage(string name, string containerName);
+        Task<IEnumerable<string>> AllImages(string containerName);
+        Task<bool> UploadImage(string name, IFormFile file, string containerName);
+        Task<bool> DeleteImage(string name, string containerName);
+    }
+}
